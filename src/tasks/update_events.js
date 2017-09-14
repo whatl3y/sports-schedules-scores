d98c0dd7398bc1b3c29491701a6b1aa8a2cd9f3e
Lance Whatley <whatl3y@gmail.com>
2017-09-14 17:04:24 -0400

    Adjust spacing

--- src/tasks/update_events.js
+++ src/tasks/update_events.js
@@ -46,94 +46,93 @@ const api = CfbApi()
         return
 
       await Promise.each(apiResponse.games, async game => {
-          try {
-            teams.resetRecord()
-            events.resetRecord()
+        try {
+          teams.resetRecord()
+          events.resetRecord()
 
-            log.debug("Updating event", game)
+          log.debug("Updating event", game)
 
-            // Check teams for game and whether they exist in the database,
-            // insert them if not, otherwise move on to the event data
-            const homeTeam = game.homeTeam
-            const awayTeam = game.awayTeam
+          // Check teams for game and whether they exist in the database,
+          // insert them if not, otherwise move on to the event data
+          const homeTeam = game.homeTeam
+          const awayTeam = game.awayTeam
 
-            log.debug("Home Team", homeTeam)
-            log.debug("Away Team", awayTeam)
-            const homeExists = await teams.findByColumn(homeTeam.location, 'location')
-            if (!homeExists) {
-              const bufferHome = await api.getImageBuffer(homeTeam.logoUrl)
-              let imageNameHome = homeTeam.logoUrl.split('/')
-              imageNameHome = `ncaaf/${imageNameHome[imageNameHome.length - 1]}`
-              await aws.writeFile({data: bufferHome, exact_filename: true, filename: imageNameHome})
+          log.debug("Home Team", homeTeam)
+          log.debug("Away Team", awayTeam)
+          const homeExists = await teams.findByColumn(homeTeam.location, 'location')
+          if (!homeExists) {
+            const bufferHome = await api.getImageBuffer(homeTeam.logoUrl)
+            let imageNameHome = homeTeam.logoUrl.split('/')
+            imageNameHome = `ncaaf/${imageNameHome[imageNameHome.length - 1]}`
+            await aws.writeFile({data: bufferHome, exact_filename: true, filename: imageNameHome})
 
-              teams.setRecord({
-                location:                 homeTeam.location,
-                name:                     homeTeam.name,
-                full_name:                homeTeam.displayName,
-                abbreviation:             homeTeam.abbreviation,
-                team_color:               homeTeam.color,
-                logo_url:                 homeTeam.logoUrl,
-                logo_local_filename:      imageNameHome,
-                stats_url:                homeTeam.links.stats,
-                schedule_url:             homeTeam.links.schedule,
-                scores_url:               homeTeam.links.scores,
-                conference_abbreviation:  homeTeam.conference.abbreviation,
-                conference_name:          homeTeam.conference.name
-              })
-              log.debug("Home Team to insert", teams.record)
-              await teams.save('location')
-            }
-            const homeTeamId = teams.record.id
-
-            teams.resetRecord()
-
-            const awayExists = await teams.findByColumn(awayTeam.location, 'location')
-            if (!awayExists) {
-              const bufferAway = await api.getImageBuffer(awayTeam.logoUrl)
-              let imageNameAway = awayTeam.logoUrl.split('/')
-              imageNameAway = `ncaaf/${imageNameAway[imageNameAway.length - 1]}`
-              await aws.writeFile({data: bufferAway, exact_filename: true, filename: imageNameAway})
-
-              teams.setRecord({
-                location:                 awayTeam.location,
-                name:                     awayTeam.name,
-                full_name:                awayTeam.displayName,
-                abbreviation:             awayTeam.abbreviation,
-                team_color:               awayTeam.color,
-                logo_url:                 awayTeam.logoUrl,
-                logo_local_filename:      imageNameAway,
-                stats_url:                awayTeam.links.stats,
-                schedule_url:             awayTeam.links.schedule,
-                scores_url:               awayTeam.links.scores,
-                conference_abbreviation:  awayTeam.conference.abbreviation,
-                conference_name:          awayTeam.conference.name
-              })
-              log.debug("Away Team to insert", teams.record)
-              await teams.save('location')
-            }
-            const awayTeamId = teams.record.id
-
-            // Handle inserting / updating events
-            events.setRecord({
-              espn_event_id:        parseInt(game.id),
-              home_team_id:         homeTeamId,
-              visiting_team_id:     awayTeamId,
-              home_team_score:      game.scores.home,
-              visiting_team_score:  game.scores.away,
-              current_period:       game.status.period,
-              current_clock:        game.status.clock,
-              event_status:         game.status.type,
-              odds_spread:          game.odds.spread,
-              odds_over_under:      game.odds.overUnder,
-              event_timestamp:      moment.utc(game.date).toDate()
+            teams.setRecord({
+              location:                 homeTeam.location,
+              name:                     homeTeam.name,
+              full_name:                homeTeam.displayName,
+              abbreviation:             homeTeam.abbreviation,
+              team_color:               homeTeam.color,
+              logo_url:                 homeTeam.logoUrl,
+              logo_local_filename:      imageNameHome,
+              stats_url:                homeTeam.links.stats,
+              schedule_url:             homeTeam.links.schedule,
+              scores_url:               homeTeam.links.scores,
+              conference_abbreviation:  homeTeam.conference.abbreviation,
+              conference_name:          homeTeam.conference.name
             })
-            await events.save('espn_event_id')
-
-          } catch(err) {
-            log.error("Error processing event", err)
+            log.debug("Home Team to insert", teams.record)
+            await teams.save('location')
           }
+          const homeTeamId = teams.record.id
+
+          teams.resetRecord()
+
+          const awayExists = await teams.findByColumn(awayTeam.location, 'location')
+          if (!awayExists) {
+            const bufferAway = await api.getImageBuffer(awayTeam.logoUrl)
+            let imageNameAway = awayTeam.logoUrl.split('/')
+            imageNameAway = `ncaaf/${imageNameAway[imageNameAway.length - 1]}`
+            await aws.writeFile({data: bufferAway, exact_filename: true, filename: imageNameAway})
+
+            teams.setRecord({
+              location:                 awayTeam.location,
+              name:                     awayTeam.name,
+              full_name:                awayTeam.displayName,
+              abbreviation:             awayTeam.abbreviation,
+              team_color:               awayTeam.color,
+              logo_url:                 awayTeam.logoUrl,
+              logo_local_filename:      imageNameAway,
+              stats_url:                awayTeam.links.stats,
+              schedule_url:             awayTeam.links.schedule,
+              scores_url:               awayTeam.links.scores,
+              conference_abbreviation:  awayTeam.conference.abbreviation,
+              conference_name:          awayTeam.conference.name
+            })
+            log.debug("Away Team to insert", teams.record)
+            await teams.save('location')
+          }
+          const awayTeamId = teams.record.id
+
+          // Handle inserting / updating events
+          events.setRecord({
+            espn_event_id:        parseInt(game.id),
+            home_team_id:         homeTeamId,
+            visiting_team_id:     awayTeamId,
+            home_team_score:      game.scores.home,
+            visiting_team_score:  game.scores.away,
+            current_period:       game.status.period,
+            current_clock:        game.status.clock,
+            event_status:         game.status.type,
+            odds_spread:          game.odds.spread,
+            odds_over_under:      game.odds.overUnder,
+            event_timestamp:      moment.utc(game.date).toDate()
+          })
+          await events.save('espn_event_id')
+
+        } catch(err) {
+          log.error("Error processing event", err)
         }
-      )
+      })
     })
 
     log.info("Successfully updated events!")
