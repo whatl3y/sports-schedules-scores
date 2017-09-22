c0fb7d26efaab23490583d58d2d8da150933205f
Lance Whatley <whatl3y@gmail.com>
2017-09-22 14:59:05 -0400

    Add geolocation support for IP addresses

--- src/routes/999.._[star]..get.js
+++ src/routes/999.._[star]..get.js
@@ -1,4 +1,5 @@
 import bunyan from 'bunyan'
+import GeoIp from './libs/GeoIp'
 import Slack from '../libs/Slack'
 import config from '../config'
 
@@ -8,7 +9,8 @@ export default async function Index(req, res) {
   try {
     let realClientIpAddress = (req.headers['x-forwarded-for'] || req.ip).split(',')
     realClientIpAddress = realClientIpAddress[realClientIpAddress.length - 1]
-    await Slack.send(`Someone visited the main page -- IP: ${realClientIpAddress}, hostname: ${req.hostname}, User-Agent: ${req.headers['user-agent']}`)
+    const location = await GeoIp.location(realClientIpAddress)
+    await Slack.send(`Someone visited the main page -- IP: ${realClientIpAddress} (location: ${location.city}, ${location.region_code}), hostname: ${req.hostname}, User-Agent: ${req.headers['user-agent']}`)
   } catch(err) {
     log.error("Error sending slack message", err)
   }
